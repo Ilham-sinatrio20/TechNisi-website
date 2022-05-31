38466d6ae0d68d46c756d974ccbc0d055f39a1d9
merthaindri <merthaindri12@gmail.com>
2022-05-31 08:39:50 +0700

    update layout dan notif

--- resources/views/notifikasi.blade.php
+++ resources/views/notifikasi.blade.php
@@ -14,7 +14,7 @@
                 <div class="box-body p-0">
                     <div class="p-3 d-flex align-items-center bg-light border-bottom osahan-post-header">
                         <div class="dropdown-list-image mr-3">
-                            <img class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="" />
+                            <img class="rounded-circle" src="https://i.pinimg.com/originals/91/a6/b1/91a6b1001107a5c6a682ef1715c8422b.jpg" alt="" />
                         </div>
                         <div class="font-weight-bold mr-3">
                             <div class="text-truncate">Pesanan Masuk</div>
@@ -35,7 +35,7 @@
                     </div>
                     <div class="p-3 d-flex align-items-center bg-light border-bottom osahan-post-header">
                         <div class="dropdown-list-image mr-3">
-                            <img class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="" />
+                            <img class="rounded-circle" src="https://i.pinimg.com/736x/24/6f/85/246f85290e48c8b559cad272c5170cba.jpg" alt="" />
                         </div>
                         <div class="font-weight-bold mr-3">
                             <div class="text-truncate">Pesanan Masuk</div>
@@ -56,7 +56,7 @@
                     </div>
                     <div class="p-3 d-flex align-items-center bg-light border-bottom osahan-post-header">
                         <div class="dropdown-list-image mr-3">
-                            <img class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="" />
+                            <img class="rounded-circle" src="https://th.bing.com/th/id/OIP.UfmefPnHZ31Hu9oyA8P2CAAAAA?pid=ImgDet&w=299&h=299&rs=1" alt="" />
                         </div>
                         <div class="font-weight-bold mr-3">
                             <div class="text-truncate">Pesanan Diproses</div>
@@ -84,7 +84,7 @@
                 <div class="box-body p-0">
                 <div class="p-3 d-flex align-items-center bg-light border-bottom osahan-post-header">
                         <div class="dropdown-list-image mr-3">
-                            <img class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="" />
+                            <img class="rounded-circle" src="https://peo.gov.au/public/assets/pass-the-bill/images/people/president.png" alt="" />
                         </div>
                         <div class="font-weight-bold mr-3">
                             <div class="text-truncate">Pesanan Selesai</div>
@@ -104,7 +104,7 @@
                         </span>
                     </div>
                     <div class="p-3 d-flex align-items-center border-bottom osahan-post-header">
-                        <div class="dropdown-list-image mr-3"><img class="rounded-circle" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="" /></div>
+                        <div class="dropdown-list-image mr-3"><img class="rounded-circle" src="https://a0.anyrgb.com/pngimg/456/1028/topcoder-programming-language-source-code-programmer-software-developer-computer-programming-software-development-avatar-icon-design-conversation-thumbnail.png" alt="" /></div>
                         <div class="font-weight-bold mr-3">
                             <div class="text-truncate">Pesanan Selesai</div>
                             <div class="small">M.Arief: Terima kasih Teknisi!</div>
